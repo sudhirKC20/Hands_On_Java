1f884ecfa5d3295f2e103f2b31b3cc0846e83bd8
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-03 23:04:58 +0530

    commit

---
+++ DSA/src/com/StringPrograms/Panagram.java
@@ -0,0 +1,17 @@
+package com.StringPrograms;
+
+public class Panagram {
+	public static void main(String[] args) {
+		String s = "A Quick Brown Fox jumps Over The Lazy Dog";
+		System.out.println(panag(s));;
+		
+	}
+	public static Boolean panag(String s) {
+		if(s.length()<26) return false;
+		s=s.toLowerCase();
+		for(char ch = 'a'; ch<='z'; ch++) {
+			if(!s.contains(ch+"")) return false;
+		}
+		return true;
+	}
+}
