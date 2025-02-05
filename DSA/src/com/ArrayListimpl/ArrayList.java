43d28d34912521560a672e194087405bf4d96e25
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-05 23:45:23 +0530

    ArrayList 1st commit

---
+++ DSA/src/com/ArrayListimpl/ArrayList.java
@@ -0,0 +1,42 @@
+package com.ArrayListimpl;
+
+public class ArrayList {
+	Object[] a = new Object[5];
+	int c=0;
+	
+	public void add(Object e) {
+		if(c>=a.length) increase();
+		a[c++]=e;
+	}
+	
+	public void add(int index, Object e) {
+		if(index<=-1 || index>=size())
+			throw new IndexOutOfBoundsException();
+		if(c>=a.length) increase();
+		for(int i=size()-1; i>=index; i++) {
+			a[i+1]= a[i];
+		}
+		a[index]=e;
+		c++;
+	}
+	
+	public int size() {
+		return c;
+	}
+	
+	public void increase() {
+		Object[] temp = new Object[a.length+3];
+		for(int i=0; i<=a.length; i++) {
+			temp[i]=a[i];
+		}
+		a=temp;
+	}
+	
+	public Object get(int index) {
+		if(index<=-1 || index>=size())
+			throw new IndexOutOfBoundsException();
+		return a[index];
+	}
+	
+
+}
