9573fe7204a4610019f88209da3a6aadcf0a0d65
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-07 01:28:09 +0530

    ArrayStack commit

---
+++ DSA/src/com/ArrayStackimpl/ArrayStack.java
@@ -0,0 +1,44 @@
+package com.ArrayStackimpl;
+
+import java.util.EmptyStackException;
+
+public class ArrayStack {
+	private Object[] a = new Object[5];
+	private int c = 0;
+	
+	public int size() {
+		return c;
+	}
+	
+	public boolean isEmpty() {
+		return size()==0;
+	}
+	
+	public void push(Object e) {
+		if(c>=a.length) increase();
+		a[c++]=e;
+		
+	}
+	
+	public Object pop() {
+		if(isEmpty()) throw new EmptyStackException();
+		c--;
+		Object e= a[c];
+		a[c]=null;
+		return e;
+	}
+	
+	public Object peek() {
+		if(isEmpty()) throw new EmptyStackException();
+
+		return a[c-1];
+	}
+	
+	public void increase() {
+		Object [] temp= new Object[a.length+3];
+		for(int i=0; i<a.length; i++) {
+			temp[i]=a[i];
+		}
+		a=temp;
+	}
+}
---
+++ DSA/src/com/ArrayStackimpl/Test.java
@@ -0,0 +1,27 @@
+package com.ArrayStackimpl;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayStack s = new ArrayStack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.push(40);
+		s.push(50);
+		s.push(60);
+		
+		
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+
+		System.out.println(s.peek());
+
+	}
+
+}
