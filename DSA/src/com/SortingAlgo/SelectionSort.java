2a0122fb0253eaf82a0c565f507b87c10de655bf
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-11 21:47:49 +0530

    2nd commit

--- DSA/src/com/SortingAlgo/SelectionSort.java
+++ DSA/src/com/SortingAlgo/SelectionSort.java
@@ -6,19 +6,23 @@ public class SelectionSort {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the numbers to an array: ");
 		int[] a = new int[9];
+		
 		for(int i=0; i<a.length; i++) {
 			a[i]=sc.nextInt();
 		}
+		
 		System.out.println(Arrays.toString(a));
 		sort(a);
 		System.out.println(Arrays.toString(a));
 	}
+	
 	public static void sort(int[] a) {
 		for(int i=0; i<a.length; i++) {
 			int index=i;
 			for(int j=i+1; j<a.length; j++) {
 				if(a[j]<a[index]) index=j;
 			}
+			
 			if(i!=index) {
 				int temp=a[index];
 				a[index] = a[i];
