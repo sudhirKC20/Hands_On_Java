7824fa353121be1bb138327831d6d60a86b0dcba
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-13 22:51:05 +0530

    diamond pattern

--- DSA/src/com/PatternPrograms/DiamondPattern.java
+++ DSA/src/com/PatternPrograms/DiamondPattern.java
@@ -13,10 +13,12 @@ public class DiamondPattern {
 			for(int j=0; j<space; j++) {
 				System.out.print(" ");
 			}
+			
 			for(int j=0; j<star; j++) {
 				System.out.print("*");
 				
 			}
+			
 			System.out.println();
 			if(i<=n/2) {
 				space--;
