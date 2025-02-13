c0ceb7ec648ea24aaeda38f57b1a0d37768abb8a
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-13 22:39:12 +0530

    Missing nums in an array

---
+++ DSA/src/com/StringPrograms/MissingNums.java
@@ -0,0 +1,22 @@
+package com.StringPrograms;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingNums {
+
+	public static void main(String[] args) {
+		Integer[] arr = {1,3,5,7};
+		List<Integer> l1 = Arrays.asList(arr);
+		
+		int max = arr[0], min = arr[0];
+		for(Integer i : arr) {
+			if(i>max) max=i;
+			else if(i<min) min=i;
+		}
+		for(int i=min; i<max; i++) {
+			if(!l1.contains(i)) System.out.println(i);
+		}
+	}
+
+}
