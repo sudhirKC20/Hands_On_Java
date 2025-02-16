ec3c80361ca58dcc7ade15b0a6505c124d7a8c4f
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-16 21:49:08 +0530

    Rotate commit

--- DSA/src/com/StringPrograms/Rotate.java
+++ DSA/src/com/StringPrograms/Rotate.java
@@ -5,16 +5,18 @@ public class Rotate {
 	public static void main(String[] args) {
 		String s = "sudhirkumar";
 		//char[] ch = s.toCharArray();
-		 
+		
 		rotate(s,5);
-
 	}
+	
 	public static void rotate(String s, int key) {
 		if(key<0) return;
 		char[] ch = s.toCharArray();
 		char[] temp = new char[ch.length];
+		
 		for(int i=0; i<ch.length; i++) {
 			int j=i+key;
+			
 			if(j>=ch.length) {
 				j=j-ch.length;
 				temp[j]=ch[i];
