562dfdc33abe387e8ca07e0bea937c7d211dc461
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-21 22:20:50 +0530

    fibonacci series commit

---
+++ Basic_Programs/src/com/NumberPrograms/Automorphic.java
@@ -0,0 +1,27 @@
+package com.NumberPrograms;
+import java.util.*;
+
+public class Automorphic {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		 int n = sc.nextInt();
+		 int temp = n;
+		 int sq = n*n;
+		 boolean flag = true;
+		 
+		 while(n!=0) {
+			 if(n%10!=sq%10) {
+				 flag = false;
+				 break;
+			 }
+			 n/=10;
+			 sq/=10;
+		 }
+		 if(flag) System.out.println(temp + " is an automorphic number.");
+		 else System.out.println(temp + " is not an automorphic number.");
+		
+	}
+
+}
