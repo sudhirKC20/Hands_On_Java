e19d30f09689526ac3893f40e0f38fb64a7521ca
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-22 23:20:41 +0530

    commit

--- Basic_Programs/src/com/NumberPrograms/Automorphic.java
+++ Basic_Programs/src/com/NumberPrograms/Automorphic.java
@@ -6,6 +6,7 @@ public class Automorphic {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number: ");
+		
 		 int n = sc.nextInt();
 		 int temp = n;
 		 int sq = n*n;
@@ -19,7 +20,6 @@ public class Automorphic {
 			 n/=10;
 			 sq/=10;
 		 }
-		 
 		 if(flag) System.out.println(temp + " is an automorphic number.");
 		 else System.out.println(temp + " is not an automorphic number.");
 		
