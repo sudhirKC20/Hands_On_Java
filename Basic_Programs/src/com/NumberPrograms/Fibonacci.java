cf223de4e09437723f8dd9b6fc10281b50d7649e
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-22 23:21:14 +0530

    commit

--- Basic_Programs/src/com/NumberPrograms/Fibonacci.java
+++ Basic_Programs/src/com/NumberPrograms/Fibonacci.java
@@ -9,13 +9,11 @@ public class Fibonacci {
 		int n = sc.nextInt();
 		int a = 0;
 		int b = 1;
-
 		for(int i = 0 ; i < n ; i++) {
 			System.out.println(a);
 			int c = a+b;
 			a=b;
-			b=c;
-			
+			b=c;	
 		}
 		
 	}
