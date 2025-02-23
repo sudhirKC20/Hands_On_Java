a021f718627c5621194ed99a2bca2db25bb0c676
sudhirKC20 <sudhirkumarc5078@gmail.com>
2025-02-23 22:32:15 +0530

    commit

--- DSA/src/com/ArrayPrograms/Program2.java
+++ DSA/src/com/ArrayPrograms/Program2.java
@@ -7,6 +7,7 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		int[] a = {3,5,6,2,1,8,7,6,3,4,45,78};
+		
 		for(int i=a.length-1; i>0; i--) {
 			if(i==a.length-2) {
 				System.out.println(a[i]);
